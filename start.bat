a93449e7e15714ebd226f0f39cc97714e1164c08
David L Valentine <davidlewisvalentine@gmail.com>
2025-11-18 14:32:46 -0600

    hostname

--- start.bat
+++ start.bat
@@ -2,4 +2,4 @@ echo OFF
 cls
 REM to test: ssh player@localhost -p 2222
 echo Starting rl-server...
-docker run -d --name rl-server -p 2222:22 rl-server
+docker run -d --hostname rl-server --name rl-server -p 2222:22 rl-server
